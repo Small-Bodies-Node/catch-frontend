444adee3a232c6056e9f5caa9278a0a3b8197b95
d-w-d <ddarg@umd.edu>
2025-01-22 12:39:02 -0500

    Improve streaming messages; disable moving-view FITS for now

--- src/app/components/streaming-messages/streaming-messages.component.ts
+++ src/app/components/streaming-messages/streaming-messages.component.ts
@@ -9,6 +9,8 @@ import { selectApiDataStatus } from '../../ngrx/selectors/api-data.selectors';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { IApiDataStatus } from '../../../models/IApiDataStatus';
 import { controlLabelsDictForSources } from '../../../models/TControlKeyForSources';
+import { IApiFixedStatus } from '../../../models/IApiFixedStatus';
+import { selectApiFixedStatus } from '../../ngrx/selectors/api-fixed.selectors';
 
 @Component({
   selector: 'app-streaming-messages',
@@ -25,23 +27,34 @@ export class StreamingMessagesComponent implements OnInit, OnDestroy {
   padding = 0;
   sources: string[] = [];
 
-  private apiStatusSubscription?: Subscription;
+  msgs: string[] = [];
+
+  subscriptions: Subscription = new Subscription();
 
   constructor(
     private store$: Store<IAppState>,
     private cdr: ChangeDetectorRef
-  ) {}
+  ) {
+    this.subscriptions.add(
+      this.store$.select(selectApiDataStatus).subscribe((status) => {
+        this.updateMovingStreamingMessage(status);
+      })
+    );
 
-  ngOnInit(): void {
-    this.apiStatusSubscription = this.store$
-      .select(selectApiDataStatus)
-      .subscribe((status) => {
-        // Use runOutsideAngular if the updates are extremely frequent
-        this.updateStreamingMessage(status);
-      });
+    this.subscriptions.add(
+      this.store$.select(selectApiFixedStatus).subscribe((status) => {
+        this.updateFixedStreamingMessage(status);
+      })
+    );
   }
 
-  private updateStreamingMessage(status: IApiDataStatus): void {
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  private updateMovingStreamingMessage(status: IApiDataStatus): void {
     this.streamingMessage = status.message || '';
     this.streamingCode = status.code;
 
@@ -50,20 +63,39 @@ export class StreamingMessagesComponent implements OnInit, OnDestroy {
     const { target, isCached, isUncertaintyEllipse, padding, sources } =
       status.query;
 
-    this.target = target;
-    this.isCached = isCached;
-    this.isUncertaintyEllipse = isUncertaintyEllipse;
-    this.padding = padding;
-
-    // Convert sources to user-presentable names
+    this.msgs = [
+      `Target: ` + target,
+      `Cached: ` + isCached,
+      `Uncertainty ellipse: ` + isUncertaintyEllipse,
+      `Padding: ` + padding,
+      `Sources: ` +
+        sources.map((source) => controlLabelsDictForSources[source]),
+    ];
     this.sources = sources.map((source) => controlLabelsDictForSources[source]);
 
     // Explicitly trigger change detection
     this.cdr.detectChanges();
   }
 
-  ngOnDestroy(): void {
-    // Properly unsubscribe to prevent memory leaks
-    this.apiStatusSubscription?.unsubscribe();
+  private updateFixedStreamingMessage(status: IApiFixedStatus): void {
+    this.streamingMessage = status.message || '';
+    this.streamingCode = status.code;
+    if (!status.query) return;
+
+    const { ra, dec, intersectionType, radius, startTime, stopTime, sources } =
+      status.query;
+
+    this.msgs = [
+      `ra: ` + ra,
+      `dec: ` + dec,
+      `IntersectionType: ` + intersectionType,
+      `Radius: ` + radius,
+      `StartTime: ` + (startTime || 'N/A'),
+      `StopTime: ` + (stopTime || 'N/A'),
+    ];
+    this.sources = sources.map((source) => controlLabelsDictForSources[source]);
+
+    // Explicitly trigger change detection
+    this.cdr.detectChanges();
   }
 }
