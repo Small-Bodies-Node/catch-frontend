85070fa4bd442aae17899703b503f802f30ced23
d-w-d <ddarg@umd.edu>
2025-05-15 09:37:42 -0400

    Porting WCS II

--- src/app/data-display/table-thumbnail/table-thumbnail.component.ts
+++ src/app/data-display/table-thumbnail/table-thumbnail.component.ts
@@ -84,6 +84,7 @@ export class TableThumbnailComponent implements OnChanges {
    * Some surveys need to be flipped, etc. to have ra and dec increase +ve in consistent dirn
    */
   getSurveyScaleTransform() {
+    return 'scale(1, 1)';
     if (!this.isReorientated) return 'scale(1, 1)';
     if (!this.apiDatum) return 'scale(1, 1)';
     if (this.apiDatum.source === 'neat_palomar_tricam') {
--- src/models/IEnvironment.ts
+++ src/models/IEnvironment.ts
@@ -10,5 +10,5 @@ export interface IEnvironment {
   };
 
   //
-  serverApiBaseUrl: '/';
+  serverApiBaseUrl: string;
 }
